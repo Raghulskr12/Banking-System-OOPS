f8f4ba88345f15c86250360f210b0718188df433
Raghulskr12 <kalairaghul70@gmail.com>
2026-01-11 22:35:09 +0530

    [ADD] Account class model updated with obj,construstor,methods

--- src/models/Account.java
+++ src/models/Account.java
@@ -1,4 +1,31 @@
 package src.models;
 
-public class Account {
+public abstract class Account {
+    private int accountNumber;
+    private Customer customer;
+    private double balance;
+
+    Account(int accountNumber, Customer customer, double balance){
+        this.accountNumber=accountNumber;
+        this.customer=customer;
+        this.balance=balance;
+    }
+
+    public int getAccountNumber()
+    {
+        return accountNumber;
+    }
+    public Customer getCustomer()
+    {
+        return customer;
+    }
+    public double getBalance()
+    {
+        return balance;
+    }
+
+    public abstract void deposit(double amount);
+    public abstract boolean withdraw(double amount);
+
+
 }
