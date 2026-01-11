3d8ebcc17d3b7ae2b40d503ea4c40e4fee090a4a
Raghulskr12 <kalairaghul70@gmail.com>
2026-01-11 22:20:50 +0530

    [ADD] objects constructor getters and setters added for the customer

--- src/models/Customer.java
+++ src/models/Customer.java
@@ -1,4 +1,31 @@
 package src.models;
 
 public class Customer {
+    public String name;
+    public String phone;
+    public String address;
+
+    Customer(String name, String phone, String address)
+    {
+        this.name=name;
+        this.phone=phone;
+        this.address=address;
+    }
+
+    public String getName(){return name;}
+    public String getPhone(){return phone;}
+    public String getAddress(){return address;}
+
+    public void setName(String name){
+        this.name=name;
+    }
+    public void setPhone(String phone)
+    {
+        this.phone=phone;
+    }
+    public void setAddress(String address)
+    {
+        this.address=address;
+    }
+
 }
