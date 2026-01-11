2de73cdc5e0cfdc334c2bedd6317dddb7dd6ffb4
Raghulskr12 <kalairaghul70@gmail.com>
2026-01-11 22:41:59 +0530

    [ADD] Current  account extends the account with the methods overrided with the deposit and withdraw functions

--- src/models/CurrentAccount.java
+++ src/models/CurrentAccount.java
@@ -1,6 +1,23 @@
 package src.models;
 
 public class CurrentAccount extends Account {
+    CurrentAccount(int accountNumber, Customer customer, double balance) {
+        super(accountNumber, customer, balance);
+    }
 
+    @Override
+    public void deposit(double amount) {
+        balance+=amount;
+    }
+
+    @Override
+    public boolean withdraw(double amount) {
+        if(balance>amount)
+        {
+            balance-=amount;
+            return true;
+        }
+        return false;
+    }
 
 }
