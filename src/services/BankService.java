2606198d772cc5e514c7d0318736b8e92750b9a8
Raghulskr12 <kalairaghul70@gmail.com>
2026-01-12 10:21:44 +0530

    [ADD] Bank Service Updated with methods and objects

--- src/services/BankService.java
+++ src/services/BankService.java
@@ -1,4 +1,23 @@
 package src.services;
 
+import src.models.*;
+
+import java.util.*;
+
+
 public class BankService {
+    Map<Integer, Account> accounts = new HashMap<>();
+    private Scanner scanner= new Scanner(System.in);
+
+
+
+    public void createAccount(){}
+
+    public void depositMoney(){}
+
+    public void withdrawMoney(){}
+
+    public void checkBalance(){}
+
+
 }
