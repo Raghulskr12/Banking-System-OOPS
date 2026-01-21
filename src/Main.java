d32e05d81375f509b138d358c85022f25ff05859
Raghulskr12 <kalairaghul70@gmail.com>
2026-01-21 16:24:24 +0530

    [ADD] new feature remove account added in the application

--- src/Main.java
+++ src/Main.java
@@ -18,7 +18,8 @@ class Main {
             System.out.println("3. Withdraw money");
             System.out.println("4. Check Balance");
             System.out.println("5. View Account Details");
-            System.out.println("6.Exit");
+            System.out.println("6. Delete Account");
+            System.out.println("7.Exit");
 
             System.out.println("Enter your choice :");
             int ans = scanner.nextInt();
@@ -31,7 +32,8 @@ class Main {
                 case 3 -> bankService.withdrawMoney();
                 case 4 -> bankService.checkBalance();
                 case 5 -> bankService.viewAccountDetails();
-                case 6 -> {
+                case 6 -> bankService.removeAccount();
+                case 7 -> {
                     System.out.println("Thanks for using the system");
                     return;
                 }
